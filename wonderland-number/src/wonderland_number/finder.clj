6e32b69935e231f69cd8e2c2fd58030d8dc33a05
thifi <thifixp@gmail.com>
2015-11-10 19:44:33 +0200

    Add the wonderland-number solution

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
 (ns wonderland-number.finder)
 
+(defn set-str [n]
+  (set (str n)))
+
+(defn exp-10 [n]
+  (apply * (repeat n 10)))
+
+(defn wonderland-number? [n]
+  (->> (range 1 7)
+       (map * (repeat n))
+       (map set-str)
+       (apply =)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [r (range (exp-10 5) (exp-10 6))]
+    (first (filter wonderland-number? r))))
