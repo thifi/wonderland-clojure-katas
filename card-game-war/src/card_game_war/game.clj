d8e4ba205de8bcd1cf6fa58f9b7bf9749083b4ac
thifi <thifixp@gmail.com>
2015-11-22 20:56:56 +0200

    Add the card-game-war solution

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,50 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn rank-index [card]
+  (.indexOf ranks (second card)))
 
-(defn play-game [player1-cards player2-cards])
+(defn suit-index [card]
+  (.indexOf suits (first card)))
+
+(defn higher-suit [player1-card player2-card]
+  (let [suit1 (suit-index player1-card)
+        suit2 (suit-index player2-card)]
+    (cond
+      (> suit1 suit2) :player1
+      (< suit1 suit2) :player2)))
+
+(defn higher-player [player1-card player2-card]
+  (let [rank1 (rank-index player1-card)
+        rank2 (rank-index player2-card)]
+    (cond
+      (> rank1 rank2) :player1
+      (< rank1 rank2) :player2
+      (= rank1 rank2) (higher-suit player1-card player2-card))))
+
+(defn play-round [player1-card player2-card]
+  (let [cards [player1-card player2-card]
+        high-player (apply higher-player cards)]
+    (case high-player
+      :player1 [cards nil]
+      :player2 [nil cards])))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [p1 player1-cards
+         p2 player2-cards]
+    (let [empty-map (map empty? [p1 p2])]
+      (if (some true? empty-map)
+        (case empty-map
+          [false true] :player1
+          [true false] :player2)
+        (let [round-result (play-round (first p1) (first p2))
+              rest-decks [(rest p1) (rest p2)]
+              [next-p1 next-p2] (map
+                                 #(when-not (nil? %2) (conj %1 %2))
+                                 rest-decks round-result)]
+          (recur next-p1 next-p2))))))
+
+(defn deal-cards []
+  (let [half (/ (count cards) 2)
+        shuffled (shuffle cards)]
+    (split-at half shuffled)))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,50 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
+(defn assert-round [p1 p2 winner]
+  (let [res (play-round p1 p2)]
+    (is
+     (case winner
+       :player1 (= res [[p1 p2] nil])
+       :player2 (= res [nil [p1 p2]])))))
 
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (assert-round [:heart 2] [:diamond 3] :player2))
+  (testing "queens are higher rank than jacks"
+    (assert-round [:club :king] [:spade :jack] :player1))
+  (testing "kings are higher rank than queens"
+    (assert-round [:club :queen] [:spade :king] :player2))
+  (testing "aces are higher rank than kings"
+    (assert-round [:spade :ace] [:club :king] :player1))
+  (testing "if the ranks are equal, clubs beat spades"
+    (assert-round [:spade :ace] [:club :ace] :player2))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (assert-round [:club :ace] [:diamond :ace] :player2))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (assert-round [:heart :ace] [:diamond :ace] :player1)))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is
+     (let [result (apply play-game (deal-cards))]
+       (case result
+         :player1 true
+         :player2 true
+         false))))
+  (testing "the player loses when they run out of cards"
+    (is
+     (let [half        (/ (count cards) 2)
+           ranked-deck (sort-by rank-index cards)
+           [p1 p2]     (split-at half ranked-deck)
+           result      (play-game p1 p2)]
+       (= result :player2))))
+  (testing "the player loses when they run out of cards"
+    (is
+     (let [half        (/ (count cards) 2)
+           ranked-deck (sort-by rank-index cards)
+           [p1 p2]     (reverse (split-at half ranked-deck))
+           result      (play-game p1 p2)]
+       (= result :player1)))))
 
